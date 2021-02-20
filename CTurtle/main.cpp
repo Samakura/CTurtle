fbd5f391394dbaf4b97fbb6d344e19fc45d04009
Samakura <bovesam87543@gmail.com>
2021-02-19 20:35:43 -0500

    formatting fix

--- CTurtle/main.cpp
+++ CTurtle/main.cpp
@@ -7,7 +7,7 @@ void printCanvas(char canvas[][20]){
     //This for loop is responsible for printing out each element of the canvas array
     for (int x = 0; x < 20; x++){
         for (int y = 0; y < 20; y++){
-            std::cout << canvas[x][y] << ' ';
+            std::cout << canvas[x][y] << "  ";
         }
         std::cout << '\n';
     }
